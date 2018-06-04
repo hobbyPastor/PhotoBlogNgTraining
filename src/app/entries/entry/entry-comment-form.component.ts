332a7a4ae2bbcce54d4456bcc64fbeafebe241d0
BERRUM Kristian <kristian.berrum@live.com>
2018-06-04 18:02:28 +0200

    Added first form for comments (Not working)

---
+++ src/app/entries/entry/entry-comment-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-entry-comment-form',
+    templateUrl: 'entry-comment-form.component.html'
+})
+export class EntryCommentFormComponent {
+
+}
