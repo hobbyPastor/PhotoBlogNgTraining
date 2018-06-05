80eb100ea47b91dfcb900a9b2a967ce2d89a94a8
BERRUM Kristian <kristian.berrum@live.com>
2018-06-05 11:03:37 +0200

    Functionality for submitting form data added

--- src/app/entries/entry/entry-comment-form.component.ts
+++ src/app/entries/entry/entry-comment-form.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Entry } from '../shared/entry.model';
 
 @Component({
     selector: 'app-entry-comment-form',
     templateUrl: 'entry-comment-form.component.html'
 })
 export class EntryCommentFormComponent {
-    name: string = "T";
-    comment: string = "T";
+    name: string = "";
+    comment: string = "";
+    @Output() onCommentAdded = new EventEmitter<{name: string; comment: string;}>();
+    onSubmit() {
+        let comment = { name: this.name, comment: this.comment };
+        this.onCommentAdded.emit(comment);
+    }
 }
